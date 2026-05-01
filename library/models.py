957360391b274ac1d9b925618a5e778fd87af9e2
Milan Bairagi <nmilan626@gmail.com>
2026-05-01 16:28:04 +0545

    feat: extend Library model to include staff management and book operations

--- library/models.py
+++ library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from members.models import Member
+from members.models import Member, Libarian
+from books.models import Book
 
 class Library(models.Model):
     name = models.CharField(max_length=255)
@@ -11,3 +12,23 @@ class Library(models.Model):
     @property
     def members(self):
         return Member.objects.all()
+
+    @property
+    def staff(self):
+        return Libarian.objects.all()
+
+    def add_book(self, book):
+        #TODO: Implement book addition logic
+        pass
+
+    def remove_book(self, book):
+        #TODO: Implement book removal logic
+        pass
+
+    def register_member(self, member):
+        #TODO: Implement member registration logic
+        pass
+
+    def find_book(self, title):
+        #TODO: Implement book search logic
+        pass
